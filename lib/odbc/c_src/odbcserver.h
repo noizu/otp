0c3b93075bcc6a3c8bf88ea81f1d52bbdd1630a1
Keith Brings <keith.brings@noizu.com>
2020-03-27 20:46:58 +0700

    - Standardizing formatting (One True Brace, Cuddled Elses, C style inline comments replaced with // c++ comments. Removing 80 char wrapping.) - Adding type indirection to simplfy WIN32/UNIX macro logic.  (ERL_SOCKET typedef and __db_handler__ macro added.) - Replacing trivial if/else assignments with tiernary operators. - CLion Auto Format.

--- lib/odbc/c_src/odbcserver.h
+++ lib/odbc/c_src/odbcserver.h
@@ -17,10 +17,19 @@
  *
  * %CopyrightEnd%
  *
-
  */
 
-/* ----------------------------- CONSTANTS ------------------------------*/
+
+#ifndef __ODBCSERVER_H__
+#define __ODBCSERVER_H__
+
+#if defined WIN32
+#include <winsock2.h>
+#endif
+
+//---------------------------------------------------------
+// Constants
+//---------------------------------------------------------
 
 #define MAXCOLSIZE 8001
 #define MAX_ERR_MSG 1024
@@ -36,72 +45,82 @@
 #define NEW_MSG_ARRIVED  1
 #define DEC_NUM_LENGTH 50
 
-/* 0 in this case (SQL_ATTR_CONNECTION_TIMEOUT) corresponds to erlang
-   infinity. Erlang will handle all timeouts so we do not want any in the
-   portprogram. */
-#define TIME_OUT 0  
+/*!
+ * 0 in this case (SQL_ATTR_CONNECTION_TIMEOUT) corresponds to erlang
+ * infinity. Erlang will handle all timeouts so we do not want any in the
+ * portprogram.
+ */
+#define TIME_OUT 0
 
-/* Constats defining the command protocol between the Erlang control process
-   and the port program. These constants must also be defined in the same
-   way in Erlang. */ 
-#define OPEN_CONNECTION		1
-#define CLOSE_CONNECTION	2
-#define COMMIT_TRANSACTION	3
-#define COMMIT			4
-#define ROLLBACK		5
-#define QUERY			6
-#define SELECT_COUNT		7
-#define SELECT_FIRST		8
-#define SELECT_LAST		9
-#define SELECT_NEXT		10
-#define SELECT_PREV		11
-#define SELECT			12
-#define SELECT_RELATIVE		13
-#define SELECT_ABSOLUTE		14
-#define SELECT_N_NEXT		15
-#define PARAM_QUERY		16
-#define DESCRIBE                17
-#define SHUTDOWN		18
-#define LENGTH_INDICATOR_SIZE	4
-#define INT_VALUE		1
-#define STR_VALUE		2
-#define ON		        1
-#define OFF		        2
-#define DUMMY_OFFSET            0
+/*!
+ * Constants defining the command protocol between the Erlang control process
+ * and the port program. These constants must also be defined in the same
+ * way in Erlang.
+ */
+#define OPEN_CONNECTION     1
+#define CLOSE_CONNECTION    2
+#define COMMIT_TRANSACTION  3
+#define COMMIT              4
+#define ROLLBACK            5
+#define QUERY               6
+#define SELECT_COUNT        7
+#define SELECT_FIRST        8
+#define SELECT_LAST         9
+#define SELECT_NEXT        10
+#define SELECT_PREV        11
+#define SELECT             12
+#define SELECT_RELATIVE    13
+#define SELECT_ABSOLUTE    14
+#define SELECT_N_NEXT      15
+#define PARAM_QUERY        16
+#define DESCRIBE           17
+#define SHUTDOWN           18
+#define LENGTH_INDICATOR_SIZE    4
+#define INT_VALUE       1
+#define STR_VALUE       2
+#define ON              1
+#define OFF             2
+#define DUMMY_OFFSET    0
 
-/* EXIT CODES */
-#define EXIT_ALLOC		 2
-#define EXIT_ENV		 3
-#define EXIT_CONNECTION		 4
-#define EXIT_FREE		 5
-#define EXIT_STDIN_HEADER        6 
-#define EXIT_STDIN_BODY		 7
-#define EXIT_BIN		 8
-#define EXIT_THREAD		 9
-#define EXIT_PARAM_ARRAY         10
-#define EXIT_OLD_WINSOCK	 11
-#define EXIT_SOCKET_CONNECT      12  
-#define EXIT_SOCKET_SEND_HEADER	 13
-#define EXIT_SOCKET_SEND_BODY	 14
-#define EXIT_SOCKET_RECV_MSGSIZE 15
-#define EXIT_SOCKET_SEND_MSGSIZE 16
-#define EXIT_SOCKET_RECV_HEADER	 17
-#define EXIT_SOCKET_RECV_BODY    18
-#define EXIT_COLS		 19
-#define EXIT_ROWS		 20
-#define EXIT_DESC		 21
-#define EXIT_BIND		 22
-#define EXIT_DRIVER_INFO         23
+//---------------------------------------------------------
+// Exit Codes
+//---------------------------------------------------------
+#define EXIT_ALLOC            2
+#define EXIT_ENV              3
+#define EXIT_CONNECTION       4
+#define EXIT_FREE             5
+#define EXIT_STDIN_HEADER     6
+#define EXIT_STDIN_BODY       7
+#define EXIT_BIN              8
+#define EXIT_THREAD           9
+#define EXIT_PARAM_ARRAY      10
+#define EXIT_OLD_WINSOCK      11
+#define EXIT_SOCKET_CONNECT   12
+#define EXIT_SOCKET_SEND_HEADER   13
+#define EXIT_SOCKET_SEND_BODY     14
+#define EXIT_SOCKET_RECV_MSGSIZE  15
+#define EXIT_SOCKET_SEND_MSGSIZE  16
+#define EXIT_SOCKET_RECV_HEADER   17
+#define EXIT_SOCKET_RECV_BODY     18
+#define EXIT_COLS        19
+#define EXIT_ROWS        20
+#define EXIT_DESC        21
+#define EXIT_BIND        22
+#define EXIT_DRIVER_INFO 23
 
-/* COL_SIZE */
-#define COL_SQL_SMALLINT 5
-#define COL_SQL_INTEGER 10
-#define COL_SQL_REAL 7
-#define COL_SQL_DOUBLE 15
-#define COL_SQL_TINYINT 4
+//---------------------------------------------------------
+// COL_SIZE
+//---------------------------------------------------------
+#define COL_SQL_SMALLINT   5
+#define COL_SQL_INTEGER   10
+#define COL_SQL_REAL       7
+#define COL_SQL_DOUBLE    15
+#define COL_SQL_TINYINT    4
 #define COL_SQL_TIMESTAMP 19
 
-/* Types of parameters given to param_query*/
+//---------------------------------------------------------
+// Parameter Types Give to param_query
+//---------------------------------------------------------
 #define USER_SMALL_INT 1
 #define USER_INT 2
 #define USER_DECIMAL 3
@@ -119,58 +138,100 @@
 #define USER_WLONGVARCHAR 15
 #define USER_GUID 16
 
-/*------------------------   TYPDEFS  ----------------------------------*/
 
+//---------------------------------------------------------
+// Platform Indirection
+//---------------------------------------------------------
+#ifdef WIN32
+typedef SOCKET ERL_SOCKET;
+#define __db_handle__ DWORD WINAPI
+#else
+typedef int ERL_SOCKET;
+#define __db_handle__ void
+#endif
+
+//---------------------------------------------------------
+// TYPEDEFS
+//---------------------------------------------------------
+
+/*!
+ * Byte representation
+ */
 typedef unsigned char byte;
+
+/*!
+ * Boolean representation
+ */
 typedef int Boolean;
 
+/*!
+ * Column Details
+ */
 typedef struct {
     SQLSMALLINT c;
     SQLSMALLINT sql;
     SQLUINTEGER col_size;
     SQLSMALLINT decimal_digits;
     SQLLEN len;
-    SQLLEN  strlen_or_indptr;
-    SQLLEN *strlen_or_indptr_array; 
+    SQLLEN strlen_or_indptr;
+    SQLLEN *strlen_or_indptr_array;
 } col_type;
 
+/*!
+ * Database Record Column Entry
+ */
 typedef struct {
     char *buffer;
     col_type type;
 } db_column;
 
+/*!
+ * Database Result Message
+ */
 typedef struct {
     int length;
     byte *buffer;
-    Boolean dyn_alloc; 
+    Boolean dyn_alloc;
 } db_result_msg;
 
+/*!
+ * Database Errors/Warnings
+ */
 typedef struct {
     SQLCHAR sqlState[SQL_STATE_SIZE];
     SQLINTEGER nativeError;
     byte error_msg[MAX_ERR_MSG];
 } diagnos;
 
+/*!
+ * Query Parameter Set
+ */
 typedef struct {
     col_type type;
     int offset;
     SQLUSMALLINT input_output_type;
     union {
-	byte *string;
-	SQLINTEGER *integer;
-	double *floating;
-	byte *bool;
-    }values;
+        byte *string;
+        SQLINTEGER *integer;
+        double *floating;
+        byte *bool;
+    } values;
 } param_array;
 
+/*!
+ * Parameter Processing Status
+ */
 typedef struct {
     SQLUSMALLINT params_processed;
-    SQLUSMALLINT *param_status_array;  
-} param_status; 
+    SQLUSMALLINT *param_status_array;
+} param_status;
 
+/*!
+ * Database State
+ */
 typedef struct {
-    SQLHDBC connection_handle;     
-    SQLHENV environment_handle;    
+    SQLHDBC connection_handle;
+    SQLHENV environment_handle;
     SQLHSTMT statement_handle;
     db_column *columns;
     int number_of_columns;
@@ -185,22 +246,33 @@ typedef struct {
     Boolean extended_errors;
 } db_state;
 
+/*!
+ * In our Out Type.
+ */
 typedef enum {
-	ERL_ODBC_IN, ERL_ODBC_OUT, ERL_ODBC_INOUT
+    ERL_ODBC_IN,
+    ERL_ODBC_OUT,
+    ERL_ODBC_INOUT
 } in_or_out_type;
 
-#define connection_handle(db_state) (db_state -> connection_handle)
-#define environment_handle(db_state) (db_state -> environment_handle)
-#define statement_handle(db_state) (db_state -> statement_handle)
-#define columns(db_state) (db_state -> columns)
-#define nr_of_columns(db_state) (db_state -> number_of_columns)
-#define dynamic_buffer(db_state) (db_state -> dynamic_buffer)
-#define associated_result_set(db_state) (db_state -> associated_result_set)
-#define use_srollable_cursors(db_state) (db_state -> use_srollable_cursors)
-#define tuple_row(db_state) (db_state -> tuple_row)
-#define binary_strings(db_state) (db_state -> binary_strings)
-#define exists_more_result_sets(db_state) (db_state -> exists_more_result_sets)
-#define param_query(db_state) (db_state -> param_query)
-#define out_params(db_state) (db_state -> out_params)
-#define extended_errors(db_state) (db_state -> extended_errors)
-#define extended_error(db_state, errorcode) ( extended_errors(state) ? ((char *)errorcode) : NULL )
+//---------------------------------------------------------
+// Macros | Mostly pointer field access wrappers.
+//---------------------------------------------------------
+#define connection_handle(db_state) (db_state->connection_handle)
+#define environment_handle(db_state) (db_state->environment_handle)
+#define statement_handle(db_state) (db_state->statement_handle)
+#define columns(db_state) (db_state->columns)
+#define nr_of_columns(db_state) (db_state->number_of_columns)
+#define dynamic_buffer(db_state) (db_state->dynamic_buffer)
+#define associated_result_set(db_state) (db_state->associated_result_set)
+#define use_srollable_cursors(db_state) (db_state->use_srollable_cursors)
+#define tuple_row(db_state) (db_state->tuple_row)
+#define binary_strings(db_state) (db_state->binary_strings)
+#define exists_more_result_sets(db_state) (db_state->exists_more_result_sets)
+#define param_query(db_state) (db_state->param_query)
+#define out_params(db_state) (db_state->out_params)
+#define extended_errors(db_state) (db_state->extended_errors)
+#define extended_error(db_state, error_code) ( extended_errors(state) ? ((char *)error_code) : NULL )
+
+
+#endif
